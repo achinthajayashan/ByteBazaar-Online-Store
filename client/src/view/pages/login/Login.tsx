360dc85ac0834dae98916db586d263563e19cc4f
achinthajayashan <achintha.uns.2018@gmail.com>
2024-01-12 15:28:40 +0530

    register page

--- client/src/view/pages/login/Login.tsx
+++ client/src/view/pages/login/Login.tsx
@@ -1,15 +1,16 @@
 import {Component} from "react";
+import {Link} from "react-router-dom";
 
 export class Login extends Component {
     render() {
         return (
             <>
                 <section className="w-screen h-[80vh] flex justify-center items-center">
-                    <div className="shadow w-[25%] h-96 flex flex-col justify-center">
+                    <div className="shadow w-[25%] h-auto flex flex-col justify-center pt-6 pb-6">
                         <h3 className="font-extrabold text-xl mb-8 ml-12">Login</h3>
 
                         <div>
-                            <label className="ml-12 ">Username / Email</label><br/>
+                            <label className="ml-12">Username / Email</label><br/>
                             <input type="email" className="w-80 h-10 border-2 mt-3 ml-12"/>
                         </div>
 
@@ -18,10 +19,10 @@ export class Login extends Component {
                             <input type="password" className="w-80 h-10 border-2 mt-3 ml-12"/>
                         </div>
 
-                        <button className="bg-black h-10 text-white w-80 ml-12 mt-10">
+                        <button className="bg-blue-950 h-10 text-white w-80 ml-12 mt-10">
                             LOGIN
                         </button>
-
+                        <p className="text-center mt-6">Don't Have An Account ? <br/><Link to="/signin" className="text-blue-900">Register Now</Link></p>
                     </div>
                 </section>
             </>
--- client/src/view/pages/signIn/SignIn.tsx
+++ client/src/view/pages/signIn/SignIn.tsx
@@ -1,12 +1,46 @@
 import {Component} from "react";
+import {Link} from "react-router-dom";
 
 export class SignIn extends Component {
     render() {
         return (
             <>
-            <section>
+                <section className="w-screen h-[80vh] flex justify-center items-center">
+                    <div className="shadow w-[25%] h-auto flex flex-col justify-center pt-6 pb-6">
+                        <h3 className="font-extrabold text-xl mb-8 ml-12">REGISTER</h3>
 
-            </section>
+                        <div>
+                            <label className="ml-12">Name</label><br/>
+                            <input type="text" className="w-80 h-10 border-2 mt-3 ml-12"/>
+                        </div>
+
+                        <div>
+                            <label className="ml-12">Email</label><br/>
+                            <input type="email" className="w-80 h-10 border-2 mt-3 ml-12"/>
+                        </div>
+
+                        <div>
+                            <label className="ml-12">Phone Number</label><br/>
+                            <input type="tel" className="w-80 h-10 border-2 mt-3 ml-12"/>
+                        </div>
+
+                        <div className="mt-6">
+                            <label className="ml-12 ">Password</label><br/>
+                            <input type="password" className="w-80 h-10 border-2 mt-3 ml-12"/>
+                        </div>
+
+                        <div className="mt-6">
+                            <label className="ml-12 ">Confirm Password</label><br/>
+                            <input type="password" className="w-80 h-10 border-2 mt-3 ml-12"/>
+                        </div>
+
+                        <button className="bg-blue-950 h-10 text-white w-80 ml-12 mt-10">
+                            REGISTER
+                        </button>
+                        <p className="text-center mt-6">Already An Account ? <br/><Link to="/login"
+                                                                                           className="text-blue-900">Login</Link></p>
+                    </div>
+                </section>
             </>
         );
     }
